cb897aed50e2405355f5b218e273a1aea541fa09
Max ang <maxsayr5@gmail.com>
2018-04-08 04:17:28 +0700

    import chat to server

--- socket/app/server.js
+++ socket/app/server.js
@@ -1,11 +1,9 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 const port = 8000;
+const chat = require('./chat.js').chat;
 
-io.on('connection', function(client){
-  client.on('event', function(data){});
-  client.on('disconnect', function(){});
-});
+chat(io);
 
 server.listen(port, function() {
     console.log('Socket.io server is listening on ' + port);
