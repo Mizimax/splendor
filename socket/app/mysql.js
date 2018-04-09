70f0d782cfd5ff50c406e3a5775b81efe76c218e
Max ang <maxsayr5@gmail.com>
2018-04-10 00:46:54 +0700

    change db config

--- socket/app/mysql.js
+++ socket/app/mysql.js
@@ -3,7 +3,7 @@ const connection = function(mysql) {
     return mysql.createConnection({
         host: 'db.maxang.me',
         user: 'root',
-        password: 'kIsM2e98r8Gf',
+        password: 'Lw05TNPCmqry',
         database: 'splendor'
     });
 }
