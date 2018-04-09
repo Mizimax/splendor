fce28af91fdb25f1917694a49fcabc5f7fba3ecb
Max ang <maxsayr5@gmail.com>
2018-04-10 02:28:31 +0700

    add logic login register (incomplete)

--- socket/app/server.js
+++ socket/app/server.js
@@ -4,9 +4,9 @@ const cors = require('cors')
 const mysql = require('mysql2');
 const server = app.listen(port)
 const io = require('socket.io').listen(server);
-const db = require('./mysql.js').connection(mysql);
 const chat = require('./chat.js').chat;
 const user = require('./user.js');
+const db = require('./mysql.js').connection(mysql);
 
 io.on('connection', function(socket){
     let numUsers = 0;
@@ -15,6 +15,7 @@ io.on('connection', function(socket){
 
     chat(socket, db);
     user.login(socket, db);
+    user.register(socket, db);
     
     socket.on('disconnect', function(){
         numUsers--;
