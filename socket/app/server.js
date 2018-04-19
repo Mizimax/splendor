e74652477af418194a45690eb2ce346818a3f1b6
Max ang <maxsayr5@gmail.com>
2018-04-20 00:29:33 +0700

    css grid

--- socket/app/server.js
+++ socket/app/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mysql = require('mysql2');
 const server = app.listen(port)
 const io = require('socket.io').listen(server);
+
 const chat = require('./chat.js').chat;
 const user = require('./user.js');
 const db = require('./mysql.js').connection(mysql);
