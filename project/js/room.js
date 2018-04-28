884c7d6cb393073d3cd8ac461c5627e64210bb2d
jardet <you@example.com>
2018-04-28 11:42:49 +0700

    Add file Jardet

---
+++ project/js/room.js
@@ -0,0 +1,11 @@
+var roomheadache = {
+    preload: function () {
+        game.load.image('background','assets/roombg.jpg');
+    },
+    create: function (){
+        document.body.style.backgroundImage = 'button';
+
+    }
+
+
+}
