9022ae64d392d61de932501313e9d5fafc0b0242
jardet <you@example.com>
2018-05-01 01:00:58 +0700

    room complete jardet

--- project/javas/room.js
+++ project/javas/room.js
@@ -1,42 +1,50 @@
 var roomheadache = {
     preload: function () {
         game.load.image('background','assets/roombg2.jpg');
-        game.load.image('exit','assets/exit.png');
-        game.load.image('go','assets/go.png');
+        game.load.image('exit','assets/exit2.png');
+        game.load.image('go','assets/go2.png');
         game.load.image('playerbox','assets/playerbox.png');
         game.load.image('playerOne','assets/player.jpg');
         game.load.image('info','assets/roominfo.png');
+        game.load.image('send','assets/send.png');
+        game.load.image('invite','assets/invite.png');
+        var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+
     },
     create: function (){
         game.stage.backgroundcolor = '#182d3b';
         game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.5);
-        var go = game.add.button(window.outerWidth-350,window.outerHeight-200,'go',goclick,this);
-        var exit = game.add.button(window.outerWidth-200,window.outerHeight-200,'exit',exitclick,this);
-        var playerBox = [];
-        playerBox[0] = game.add.sprite(window.outerWidth-1500,window.outerHeight-800,'playerbox');
-        playerBox[1] = game.add.sprite(window.outerWidth-900,window.outerHeight-800,'playerbox');
-        playerBox[2] = game.add.sprite(window.outerWidth-1500,window.outerHeight-500,'playerbox');
-        playerBox[3] = game.add.sprite(window.outerWidth-900,window.outerHeight-500,'playerbox');
-        playerBox[0].scale.setTo(1,0.8);
-        playerBox[1].scale.setTo(1,0.8);
-        playerBox[2].scale.setTo(1,0.8);
-        playerBox[3].scale.setTo(1,0.8);
+        
+        var playerBox = game.add.sprite(window.outerWidth-1225,window.outerHeight-800,'info')
+        playerBox.scale.setTo(1);
         var player = [];
-        player[0] = game.add.sprite(window.outerWidth-1400,window.outerHeight-700,'playerOne');
-        player[1] = game.add.sprite(window.outerWidth-800,window.outerHeight-700,'playerOne');
-        player[2] = game.add.sprite(window.outerWidth-1400,window.outerHeight-400,'playerOne');
-        player[3] = game.add.sprite(window.outerWidth-800,window.outerHeight-400,'playerOne');
-        player[0].width=150;
-        player[0].height=150;
-        player[1].width=150;
-        player[1].height=150;
-        player[2].width=150;
-        player[2].height=150;
-        player[3].width=150;
-        player[3].height=150;
-        var info = game.add.sprite(window.outerWidth-350,window.outerHeight-800,'info');
-        info.width=300;
-        info.height=625;
+        player[0] = game.add.sprite(window.outerWidth-1211,window.outerHeight-761,'playerOne');
+        player[1] = game.add.sprite(window.outerWidth-1211,window.outerHeight-687,'playerOne');
+        player[2] = game.add.sprite(window.outerWidth-1211,window.outerHeight-605,'playerOne');
+        player[3] = game.add.sprite(window.outerWidth-1211,window.outerHeight-526,'playerOne');
+        player[0].width=64;
+        player[0].height=65;
+        player[1].width=64;
+        player[1].height=65;
+        player[2].width=64;
+        player[2].height=65;
+        player[3].width=64;
+        player[3].height=65;
+        var go = game.add.button(window.outerWidth-750,window.outerHeight-349,'go',goclick,this);
+        var exit = game.add.button(window.outerWidth-1200,window.outerHeight-349,'exit',exitclick,this);
+        var invite = game.add.button(window.outerWidth-425,window.outerHeight-349,'invite',goclick,this);
+        var send = game.add.button(window.outerWidth-385,window.outerHeight-157,'send',goclick,this);
+        var text = []; 
+        text[0] = game.add.text(window.outerWidth-1205,window.outerHeight-308, "Chat", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[1] = game.add.text(window.outerWidth-1205,window.outerHeight-790, "Game Room", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[2] = game.add.text(window.outerWidth-600,window.outerHeight-790, "Game Info", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[3] = game.add.text(window.outerWidth-600,window.outerHeight-526, "Invite", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[4] = game.add.text(window.outerWidth-1125,window.outerHeight-736, "Name1", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[5] = game.add.text(window.outerWidth-1125,window.outerHeight-657, "Name2", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[6] = game.add.text(window.outerWidth-1125,window.outerHeight-578, "Name3", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[7] = game.add.text(window.outerWidth-1125,window.outerHeight-499, "Name4", { font: "16px Arial", fill: "#ffffffff", align: "center" });
+        text[8] = game.add.text(window.outerWidth-600,window.outerHeight-736, "Room Name : Palum", { font: "14px Arial", fill: "#ffffffff", align: "center" });
+        text[9] = game.add.text(window.outerWidth-600,window.outerHeight-710, "Status : Locked Room", { font: "14px Arial", fill: "#ffffffff", align: "center" });
     }
 
 
@@ -45,5 +53,5 @@ function goclick(){
     var go = prompt("peter"," SHUT UP ");
 }
 function exitclick(){
-    var go = prompt("peter"," SHUT UP ");
+    game.state.start('lobby');
 }
