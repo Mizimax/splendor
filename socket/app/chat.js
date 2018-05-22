18ea4c3a0c01828e1268b87bfa30b3aa0240906f
Max ang <maxsayr5@gmail.com>
2018-05-22 19:13:56 +0700

    remove trash

--- socket/app/chat.js
+++
@@ -1,9 +0,0 @@
-const db = require("./mysql").connect();
-
-const chat = function() {
-  socket.on("chat", function(msg) {
-    socket.broadcast.emit("chat", msg);
-  });
-};
-
-exports.chat = chat;
