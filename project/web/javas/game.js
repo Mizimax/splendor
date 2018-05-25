cc87bef627aaafdea4817a220014a0f20ff8b1a2
SMooTHiE <jojojj2012@hotmail.com>
2018-05-26 01:04:07 +0700

    old

--- project/web/javas/game.js
+++ project/web/javas/game.js
@@ -33,6 +33,7 @@ for (var i = 0; i < 6; i++) {
 var InfoPlayer = [];
 InfoPlayer[1] = {
   playerName: "Player1",
+  imagePlayer: 0,
   blueCoin: 0,
   whiteCoin: 0,
   redCoin: 0,
@@ -43,7 +44,6 @@ InfoPlayer[1] = {
   cardred: 0,
   cardgreen: 0,
   cardwhite: 0,
-  goldCoin: 0,
   score: 0
 };
 InfoPlayer[2] = {
@@ -58,7 +58,6 @@ InfoPlayer[2] = {
   cardred: 0,
   cardgreen: 0,
   cardwhite: 0,
-  goldCoin: 0,
   score: 0
 };
 InfoPlayer[3] = {
@@ -73,7 +72,6 @@ InfoPlayer[3] = {
   cardred: 0,
   cardgreen: 0,
   cardwhite: 0,
-  goldCoin: 0,
   score: 0
 };
 InfoPlayer[4] = {
@@ -88,7 +86,6 @@ InfoPlayer[4] = {
   cardred: 0,
   cardgreen: 0,
   cardwhite: 0,
-  goldCoin: 0,
   score: 0
 };
 var aImageFiles = [
@@ -535,14 +532,14 @@ var gamestate = {
     });
     button[23].inputEnabled = true;
     //RightWrong
-    button[24] = game.add.button(8 * x + 95, 3 * y - 35, "right");
+    button[24] = game.add.button(8 * x + 95, 2.5 * y - 25, "right");
     button[24].width = 30;
     button[24].height = 30;
     button[24].events.onInputDown.add(function() {
       OhRight();
     });
     button[24].inputEnabled = true;
-    button[25] = game.add.button(8 * x + 50, 3 * y - 35, "wrong");
+    button[25] = game.add.button(8 * x + 50, 2.5 * y - 25, "wrong");
     button[25].width = 30;
     button[25].height = 30;
     button[25].events.onInputDown.add(function() {
@@ -570,38 +567,31 @@ var gamestate = {
     detail[8].scale.setTo(0.8);
     detail[9] = this.game.add.sprite(8 * x - 90, 3.5 * y + 30, "lilBlue");
     detail[9].scale.setTo(0.8);
-    //    detail[10] = this.game.add.sprite(8.5 * x - 35, 3.5 * y - 15, "coinGold");
-    //    detail[10].scale.setTo(0.5);
-    text[0] = game.add.text(8 * x + 50, 3 * y, "Blue:   " + coinNum[0], {
+    text[0] = game.add.text(8 * x + 50, 2.5 * y + 11, "Blue:   " + coinNum[0], {
       font: "10px Arial",
       fill: "#ffffff",
       align: "center"
     });
-    text[1] = game.add.text(8 * x + 50, 3 * y + 11, "White: " + coinNum[1], {
+    text[1] = game.add.text(8 * x + 50, 2.5 * y + 22, "White: " + coinNum[1], {
       font: "10px Arial",
       fill: "#ffffff",
       align: "center"
     });
-    text[2] = game.add.text(8 * x + 50, 3 * y + 22, "Red:    " + coinNum[2], {
+    text[2] = game.add.text(8 * x + 50, 2.5 * y + 33, "Red:    " + coinNum[2], {
       font: "10px Arial",
       fill: "#ffffff",
       align: "center"
     });
-    text[3] = game.add.text(8 * x + 50, 3 * y + 33, "Green: " + coinNum[3], {
+    text[3] = game.add.text(8 * x + 50, 2.5 * y + 44, "Green: " + coinNum[3], {
       font: "10px Arial",
       fill: "#ffffff",
       align: "center"
     });
-    text[4] = game.add.text(8 * x + 50, 3 * y + 44, "Black:  " + coinNum[4], {
+    text[4] = game.add.text(8 * x + 50, 2.5 * y + 55, "Black:  " + coinNum[4], {
       font: "10px Arial",
       fill: "#ffffff",
       align: "center"
     });
-    /*    text[22] = game.add.text(8 * x + 50, 3 * y + 55, "Gold:   " + coinNum[5], {
-      font: "10px Arial",
-      fill: "#ffffff",
-      align: "center"
-    });*/
 
     //PlayerInfo
     text[5] = game.add.text(
@@ -730,21 +720,7 @@ var gamestate = {
       fill: "#ffffff",
       align: "center"
     });
-    /*    text[21] = game.add.text(10 * x - 100, 2.5 * y - 5, coinLeft[5] + "/5", {
-      font: "25px Arial",
-      fill: "#ffffff",
-      align: "center"
-    });
-      text[23] = game.add.text(
-      8 * x + 75,
-      3.5 * y - 10,
-      ": " + InfoPlayer[1].goldCoin,
-      {
-        font: "25px Arial",
-        fill: "#ffffff",
-        align: "center"
-      }
-    );*/
+
     //reservedCard
     button[26] = game.add.button(8 * x + 25, 3.5 * y + 30, "lilGold");
 
@@ -855,7 +831,6 @@ function cardlevel_1(card, x, y, cardID) {
       text[12].setText(": " + InfoPlayer[1].cardred);
       text[13].setText(": " + InfoPlayer[1].cardgreen);
       text[14].setText(": " + InfoPlayer[1].cardblack);
-      //      text[23].setText(": " + InfoPlayer[1].goldCoin);
       //randomCardPic
       card.destroy(card);
       randIndex = Math.floor(Math.random() * aImageFiles.length);
@@ -936,7 +911,6 @@ function cardlevel_2(card, x, y, cardID) {
       text[12].setText(": " + InfoPlayer[1].cardred);
       text[13].setText(": " + InfoPlayer[1].cardgreen);
       text[14].setText(": " + InfoPlayer[1].cardblack);
-      //      text[23].setText(": " + InfoPlayer[1].goldCoin);
       //randomCardPic
       card.destroy(card);
       randIndex2 = Math.floor(Math.random() * aImageFiles2.length);
@@ -1016,7 +990,6 @@ function cardlevel_3(card, x, y, cardID) {
       text[12].setText(": " + InfoPlayer[1].cardred);
       text[13].setText(": " + InfoPlayer[1].cardgreen);
       text[14].setText(": " + InfoPlayer[1].cardblack);
-      //      text[23].setText(": " + InfoPlayer[1].goldCoin);
       //randomCardPic
       card.destroy(card);
       randIndex3 = Math.floor(Math.random() * aImageFiles3.length);
@@ -1067,123 +1040,42 @@ function click_Noble(card, NobleID) {
 }
 function click_coin(color) {
   if (color == "blue") {
-    coinLeft[0] -= 1;
-    coinNum[0] += 1;
+    if (coinLeft[0] > 0) {
+      coinLeft[0] -= 1;
+      coinNum[0] += 1;
+    }
     text[0].setText("Blue:   " + coinNum[0]);
     text[16].setText(coinLeft[0] + "/7");
   } else if (color == "white") {
-    coinLeft[1] -= 1;
-    coinNum[1] += 1;
+    if (coinLeft[1] > 0) {
+      coinLeft[1] -= 1;
+      coinNum[1] += 1;
+    }
     text[1].setText("White: " + coinNum[1]);
     text[17].setText(coinLeft[1] + "/7");
   } else if (color == "red") {
-    coinLeft[2] -= 1;
-    coinNum[2] += 1;
+    if (coinLeft[2] > 0) {
+      coinLeft[2] -= 1;
+      coinNum[2] += 1;
+    }
     text[2].setText("Red:    " + coinNum[2]);
     text[18].setText(coinLeft[2] + "/7");
   } else if (color == "green") {
-    coinLeft[3] -= 1;
-    coinNum[3] += 1;
+    if (coinLeft[3] > 0) {
+      coinLeft[3] -= 1;
+      coinNum[3] += 1;
+    }
     text[3].setText("Green: " + coinNum[3]);
     text[19].setText(coinLeft[3] + "/7");
   } else if (color == "black") {
-    coinLeft[4] -= 1;
-    coinNum[4] += 1;
+    if (coinLeft[4] > 0) {
+      coinLeft[4] -= 1;
+      coinNum[4] += 1;
+    }
     text[4].setText("Black:  " + coinNum[4]);
     text[20].setText(coinLeft[4] + "/7");
-  } /*else if (color == "gold") {
-    coinLeft[5] -= 1;
-    coinNum[5] += 1;
-    text[21].setText(coinLeft[5] + "/5");
-  }*/
-}
-/*function OhRight() {
-  if (turn == 2) {
-    InfoPlayer[1].inputEnabled = false;
   }
-  var total =
-    InfoPlayer[turn].blueCoin +
-    InfoPlayer[turn].whiteCoin +
-    InfoPlayer[turn].redCoin +
-    InfoPlayer[turn].greenCoin +
-    InfoPlayer[turn].blackCoin;
-  if (
-    total + coinNum[0] + coinNum[1] + coinNum[2] + coinNum[3] + coinNum[4] >
-    10
-  ) {
-    testMessageBox("toomuch");
-    ohCancel();
-    if (coinNum[0] + coinNum[1] + coinNum[2] + coinNum[3] + coinNum[4] > 0) {
-      testMessageBox("format");
-      ohCancel();
-    } else if (coinNum[5] != 1) {
-      testMessageBox("Gold");
-      ohCancel();
-    } else {
-      //pick gold coin
-      InfoPlayer[1].goldCoin += coinNum[5];
-      ohCancel();
-      updatePlayerInfo();
-      console.log(turn);
-      turn += 1;
-      turn = turn % 4;
-      if (turn % 4 == 0) {
-        turn += 4;
-      }
-    }
-  } else if (
-    coinNum[0] + coinNum[1] + coinNum[2] + coinNum[3] + coinNum[4] <=
-    3
-  ) {
-    if (
-      coinNum[0] <= 1 &&
-      coinNum[1] <= 1 &&
-      coinNum[2] <= 1 &&
-      coinNum[3] <= 1 &&
-      coinNum[4] <= 1
-    ) {
-      InfoPlayer[1].blueCoin += coinNum[0];
-      InfoPlayer[1].whiteCoin += coinNum[1];
-      InfoPlayer[1].redCoin += coinNum[2];
-      InfoPlayer[1].greenCoin += coinNum[3];
-      InfoPlayer[1].blackCoin += coinNum[4];
-      ohCancel();
-      updatePlayerInfo();
-      console.log(turn);
-      turn += 1;
-      turn = turn % 4;
-      if (turn % 4 == 0) {
-        turn += 4;
-      }
-    } else if (
-      coinNum[0] + coinNum[1] + coinNum[2] + coinNum[3] + coinNum[4] < 3 &&
-      (coinNum[0] == 2 ||
-        coinNum[1] == 2 ||
-        coinNum[2] == 2 ||
-        coinNum[3] == 2 ||
-        coinNum[4] == 2)
-    ) {
-      InfoPlayer[1].blueCoin += coinNum[0];
-      InfoPlayer[1].whiteCoin += coinNum[1];
-      InfoPlayer[1].redCoin += coinNum[2];
-      InfoPlayer[1].greenCoin += coinNum[3];
-      InfoPlayer[1].blackCoin += coinNum[4];
-      ohCancel();
-      updatePlayerInfo();
-      console.log(turn);
-      turn += 1;
-      turn = turn % 4;
-      if (turn % 4 == 0) {
-        turn += 4;
-      }
-    } else {
-      testMessageBox("format");
-      ohCancel();
-    }
-  } else {
-    testMessageBox("numbe");
-    ohCancel();
-  }*/
+}
 function OhRight() {
   /*  if (turn == 2) {
       InfoPlayer[1].inputEnabled = false;
@@ -1263,7 +1155,6 @@ function OhRight() {
     text[7].setText(": " + InfoPlayer[1].redCoin);
     text[8].setText(": " + InfoPlayer[1].greenCoin);
     text[9].setText(": " + InfoPlayer[1].blackCoin);
-    //    text[23].setText(": " + InfoPlayer[1].goldCoin);
     text[0].setText("Blue:   " + coinNum[0]);
     text[1].setText("White: " + coinNum[1]);
     text[2].setText("Red:    " + coinNum[2]);
@@ -1275,7 +1166,6 @@ function OhRight() {
       InfoPlayer[1].redCoin +
       InfoPlayer[1].greenCoin +
       InfoPlayer[1].blackCoin;
-    //     + InfoPlayer[1].goldCoin;
     text[15].setText("Total " + total + "/10");
   }
 }
@@ -1290,8 +1180,6 @@ function ohCancel() {
   text[3].setText("Green: " + coinNum[3]);
   coinNum[4] = 0;
   text[4].setText("Black:  " + coinNum[4]);
-  //  coinNum[5] = 0;
-  //  text[22].setText("Gold:   " + coinNum[5]);
   coinLeft[0] =
     7 -
     InfoPlayer[1].blueCoin -
@@ -1322,25 +1210,19 @@ function ohCancel() {
     InfoPlayer[2].blackCoin -
     InfoPlayer[3].blackCoin -
     InfoPlayer[4].blackCoin;
-  /*  coinLeft[5] =
-    5 -
-    InfoPlayer[1].goldCoin -
-    InfoPlayer[2].goldCoin -
-    InfoPlayer[3].goldCoin -
-    InfoPlayer[4].goldCoin;*/
+
   text[16].setText(coinLeft[0] + "/7");
   text[17].setText(coinLeft[1] + "/7");
   text[18].setText(coinLeft[2] + "/7");
   text[19].setText(coinLeft[3] + "/7");
   text[20].setText(coinLeft[4] + "/7");
-  //  text[21].setText(coinLeft[5] + "/5");
+
   total =
     InfoPlayer[1].blueCoin +
     InfoPlayer[1].whiteCoin +
     InfoPlayer[1].redCoin +
     InfoPlayer[1].greenCoin +
     InfoPlayer[1].blackCoin;
-  //  +  InfoPlayer[1].goldCoin;
   text[15].setText("Total " + total + "/10");
 }
 function summit() {
@@ -1357,13 +1239,7 @@ function testMessageBox(type) {
     this.showMessageBox("You don't have enough coins!", 5 * x, 2 * y);
   } else if (type == "noCard") {
     this.showMessageBox("You don't have enough cards!", 5 * x, 2 * y);
-  } /* else if (type == "Gold") {
-    this.showMessageBox(
-      "You can't pick more than one gold coin!",
-      5 * x,
-      2 * y
-    );
-  }*/
+  }
 }
 function showMessageBox(text, w = 300, h = 300) {
   if (this.msgBox) {
@@ -1511,18 +1387,7 @@ function showScore(w = 500, h = 500) {
       "\n\n" +
       InfoPlayer[4].cardblack
   );
-  /*  scoreNum[10] = game.add.text(
-    0,
-    0,
-    InfoPlayer[1].goldCoin +
-      "\n\n" +
-      InfoPlayer[2].goldCoin +
-      "\n\n" +
-      InfoPlayer[3].goldCoin +
-      "\n\n" +
-      InfoPlayer[4].goldCoin
-  );*/
-  scoreNum[11] = game.add.text(
+  scoreNum[10] = game.add.text(
     0,
     0,
     InfoPlayer[1].score +
@@ -1533,7 +1398,7 @@ function showScore(w = 500, h = 500) {
       "\n\n" +
       InfoPlayer[4].score
   );
-  scoreNum[12] = game.add.text(
+  scoreNum[11] = game.add.text(
     0,
     0,
     InfoPlayer[1].playerName +
@@ -1565,7 +1430,7 @@ function showScore(w = 500, h = 500) {
   detail2[8].scale.setTo(0.7);
   detail2[9] = this.game.add.sprite(0, 0, "lilBlack");
   detail2[9].scale.setTo(0.7);
-  //  detail2[10] = this.game.add.sprite(0, 0, "coinGold").scale.setTo(0.4);
+
   back2.width = w;
   back2.height = h;
   scrBox.add(back2);
@@ -1580,9 +1445,8 @@ function showScore(w = 500, h = 500) {
   scrBox.add(scoreNum[7]);
   scrBox.add(scoreNum[8]);
   scrBox.add(scoreNum[9]);
-  //  scrBox.add(scoreNum[10]);
+  scrBox.add(scoreNum[10]);
   scrBox.add(scoreNum[11]);
-  scrBox.add(scoreNum[12]);
   scrBox.add(detail2[0]);
   scrBox.add(detail2[1]);
   scrBox.add(detail2[2]);
@@ -1593,7 +1457,6 @@ function showScore(w = 500, h = 500) {
   scrBox.add(detail2[7]);
   scrBox.add(detail2[8]);
   scrBox.add(detail2[9]);
-  //  scrBox.add(detail2[10]);
   closeButton2.x = back2.width - 40;
   closeButton2.y = 10;
   closeButton2.scale.setTo(0.5);
@@ -1621,12 +1484,10 @@ function showScore(w = 500, h = 500) {
   scoreNum[8].y = back2.height / 2 - scoreNum[8].height / 2 + 10;
   scoreNum[9].x = back2.width / 2 - scoreNum[9].width / 2 + 185;
   scoreNum[9].y = back2.height / 2 - scoreNum[9].height / 2 + 10;
-  //  scoreNum[10].x = back2.width / 2 - scoreNum[10].width / 2 + 225;
-  //  scoreNum[10].y = back2.height / 2 - scoreNum[10].height / 2 + 10;
-  scoreNum[11].x = back2.width / 2 - scoreNum[11].width / 2 + 265;
+  scoreNum[10].x = back2.width / 2 - scoreNum[10].width / 2 + 265;
+  scoreNum[10].y = back2.height / 2 - scoreNum[10].height / 2 + 10;
+  scoreNum[11].x = 20;
   scoreNum[11].y = back2.height / 2 - scoreNum[11].height / 2 + 10;
-  scoreNum[12].x = 20;
-  scoreNum[12].y = back2.height / 2 - scoreNum[12].height / 2 + 10;
   detail2[0].x = back2.width / 2 - scoreNum[0].width / 2 - 180;
   detail2[0].y = back2.height / 2 - scoreNum[0].height / 2 - 30;
   detail2[1].x = back2.width / 2 - scoreNum[1].width / 2 - 140;
@@ -1647,8 +1508,7 @@ function showScore(w = 500, h = 500) {
   detail2[8].y = back2.height / 2 - scoreNum[8].height / 2 - 30;
   detail2[9].x = back2.width / 2 - scoreNum[9].width / 2 + 180;
   detail2[9].y = back2.height / 2 - scoreNum[9].height / 2 - 30;
-  //  detail2[10].x = back2.width / 2 - scoreNum[10].width / 2 + 220;
-  //  detail2[10].y = back2.height / 2 - scoreNum[10].height / 2 - 30;
+
   this.scrBox = scrBox;
 }
 function hideBox2() {
