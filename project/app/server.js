b8f0f6bc18af2e1d1f7166e2c5c7b4a5c505fbe4
Phurithat <phurithat.59070501090@mail.kmutt.ac.th>
2018-05-27 16:59:20 +0700

    analysis

--- project/app/server.js
+++ project/app/server.js
@@ -23,6 +23,8 @@ const chat = require("./chat.js").chat;
 //const user = require("./user.js");
 const io_connect = require("./io.connect.js");
 
+
+
 auth(io);
 
 io.on("connection", async function(socket) {
@@ -63,15 +65,16 @@ app.use(cors());
 app.use(session);
 app.use(express.static(__dirname + "/../web"));
 
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
 
 app.post("/login", async function(req, res) {
   let data = req.body;
-  let [results] = await db.query("SELECT * FROM user WHERE UserName = ?", [
+  let [results] = await db.query("SELECT * FROM users WHERE UserName = ?", [
     data.username
   ]);
   if (results.length === 1) {
@@ -83,7 +86,7 @@ app.post("/login", async function(req, res) {
       req.session.save();
       //create cookie จำไว้ใน remember_token ด้วย
       let [resUpdate] = await db.query(
-        "UPDATE user SET RememberToken = ? WHERE UserID = ?",
+        "UPDATE users SET RememberToken = ? WHERE UserID = ?",
         [req.sessionID, response.UserID]
       );
       if (resUpdate.affectedRows === 1) {
@@ -193,5 +196,77 @@ app.get('/problem_info', async function(req, res){
     }
 );
 });
+
+app.get('/analysis', async function(req, res){
+
+  let [ one ] = await db.query(
+    "SELECT u.user_name, u.user_display_name, COUNT(*) AS TotalGameRound FROM user u, game_match g, match_player m  WHERE u.user_id = m.user_id AND m.match_id = g.match_id GROUP BY m.user_id ORDER BY TotalGameRound LIMIT 10;"
+  );
+  let [two] = await db.query(
+    "SELECT u.user_name, u.user_display_name, COUNT(*) AS Win FROM user u, game_match g, match_player m  WHERE u.user_id = m.user_id AND m.match_id = g.match_id AND m.score >= 15 GROUP BY m.match_id, m.user_id ORDER BY Win LIMIT 10;"
+  );
+  let [three] = await db.query(
+    "SELECT match_id, (match_end - match_start)/60 AS m_time from game_match WHERE match_type = 'RANK' AND match_status = 'END' ORDER BY m_time DESC LIMIT 10;"
+  );
+  //
+  let [four] = await db.query(
+    "select match_id, (match_end - match_start)/60 AS m_time from game_match WHERE match_type = 'NORMAL' AND match_status = 'END' ORDER BY m_time DESC LIMIT 10;"
+  );
+  let [five] = await db.query(
+    "SELECT u.user_name, u.user_display_name , COUNT(*) AS FriendAmount FROM user u, user_relation r WHERE u.user_id = r.user_id AND relation_type = 'FRIEND' GROUP BY u.user_id ORDER BY FriendAmount DESC LIMIT 1;"
+  );
+  let [six] = await db.query(
+    "SELECT user_name, user_display_name, rank_name FROM user WHERE rank_score > 0 ORDER BY rank_score DESC LIMIT 10;"
+  );
+  let [seven] = await db.query(
+    "SELECT CAST ( ( (match_start + 1)%10000000000 ) /100000000  AS INT) AS MONTH, COUNT(*) AS MatchAmount  FROM game_match WHERE match_start >0 GROUP BY MONTH ORDER BY MONTH;"
+  );
+  let [eight] = await db.query(
+    "SELECT rank_name AS RANK,COUNT(*) AS  PlayerAmount FROM user WHERE rank_score > 0 GROUP BY rank_name ORDER BY rank_score DESC LIMIT 10;"
+  );
+  // skip...
+  let [nine] = await db.query(
+    "SELECT color_name, SUM(amount) AS Amount FROM player_card q, user u, coin_color c WHERE  u.user_id = q.user_id AND c.color_id = q.color_id GROUP BY q.color_id ORDER BY q.color_id;"
+  );
+  let [ten] = await db.query(
+    "select user_name, user_display_name, COUNT(*) AS ReportAmount from problem p, user u WHERE u.user_id = p.user_id GROUP BY p.user_id ORDER BY ReportAmount DESC LIMIT 1;"
+  );
+  let [eleven] = await db.query(
+    "select  t.problem_type AS problemreporttype, COUNT(*) AS AMOUNT from problem p, problem_detail d, problem_type t WHERE p.problem_detail_id = d.problem_detail_id AND d.problem_type_id = t.problem_type_id GROUP BY t.problem_type_id;"
+  );
+  let [twelve] = await db.query(
+    "select match_type, COUNT(*) AS Amount from game_match WHERE match_status = 'END' AND match_turn > 0 GROUP BY match_type ORDER BY Amount DESC;"
+  );
+  let [thirteen] = await db.query(
+    "SELECT u.user_id , COUNT(*) AS Losetimes FROM game_match g, match_player m, user u WHERE u.user_id = m.user_id AND m.match_id = g.match_id AND g.match_status = 'END' AND m.score < 15 GROUP BY u.user_id  ORDER BY Losetimes DESC LIMIT 1;"
+  );
+  let [fourteen] = await db.query(
+    "SELECT user_role, COUNT(*) AS Amount FROM user GROUP BY user_role;"
+  );
+  let [fifteen] = await db.query(
+    "SELECT AVG ( (match_end - match_start)/60 ) AS 'AVGTime(mins)' from game_match WHERE match_type = 'RANK' AND match_status = 'END';"
+  );
+  res.json(
+    {
+      one : one,
+      two : two,
+      three : three,
+      four : four,
+      five : five,
+      six : six,
+      seven : seven,
+      eight : eight,
+      nine: nine,
+      ten : ten,
+      eleven : eleven,
+      twelve : twelve,
+      thirteen : thirteen,
+      fourteen : fourteen,
+      fifteen : fifteen
+    }
+  )
+}
+);
+
 io.use(cookieParser());
 io.use(sharedsession(session));
